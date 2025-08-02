13c8aea7c1dcd6e48697b89c104c917c45e3e128
Gordon Shannon <gordo169@gmail.com>
2025-08-02 10:42:16 -0400

    Fixing display issues on help page

--- wnttapp/src/Help.jsx
+++ wnttapp/src/Help.jsx
@@ -1,3 +1,4 @@
+import './css/Help.css'
 import { useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
@@ -5,7 +6,6 @@ import Container from 'react-bootstrap/Container'
 import { getData as getGraphData } from './tutorials/graph'
 import { getData as getMapData } from './tutorials/map'
 import Tutorial from './Tutorial'
-import { widthLessThan, SmallBase } from './utils'
 
 export default function Help() {
     const [showGraphTut, setShowGraphTut] = useState(false)
@@ -16,33 +16,26 @@ export default function Help() {
         setShowMapTut(false)
     }
 
-    const videoWidth = widthLessThan(SmallBase) ? `${window.innerWidth}px` : '560px'
-    const videoHeight = widthLessThan(SmallBase) ? `${(window.innerWidth * 9) / 16}px` : '315px'
-
     return (
         <Container>
-            <Row className='mt-md-5'>
-                <Col className='text-center'>
-                    <h4>This video covers all the basics of the Tide Tracker:</h4>
+            <Row>
+                <Col className='text-center pt-4 pb-2 titles'>
+                    This video covers all the basics of the Tide Tracker.
                 </Col>
             </Row>
-            <Row className='my-md-3'>
-                <Col className='text-center'>
+            <Row>
+                <Col className='text-center video'>
                     <iframe
-                        style={{ border: '1px solid black' }}
+                        className='video'
                         allow='fullscreen'
                         referrerPolicy='no-referrer'
-                        width={videoWidth}
-                        height={videoHeight}
                         src='https://www.youtube.com/embed/wr2nfjE43Gg?autoplay=0'></iframe>
                 </Col>
             </Row>
-            <Row className='mt-md-5'>
-                <Col className='text-center'>
-                    <h4>Slide show tutorials:</h4>
-                </Col>
+            <Row>
+                <Col className='text-center pt-4 pb-2 titles'>Slide show tutorials</Col>
             </Row>
-            <Row className='my-md-3'>
+            <Row>
                 <Col className='text-center'>
                     <Button
                         variant='primary'
