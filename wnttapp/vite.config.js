732350d3bd4430041ca90621f3dae309a07eb9a8
Gordon Shannon <gordo169@gmail.com>
2025-09-06 11:04:46 -0400

    total npm and package update

--- wnttapp/vite.config.js
+++ wnttapp/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import Inspect from 'vite-plugin-inspect'
-import eslint from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint2'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-    base: '/',
     plugins: [react(), Inspect(), eslint()],
     server: {
         port: 3001,
