0212993d16f7f866b93ab16f10614c759facf2c4
Gordon Shannon <gordo169@gmail.com>
2025-11-05 11:56:48 -0500

    Refactored address lookup popup to fix bug preventing query from >1 exec.

--- wnttapp/src/AddressPopup.jsx
+++ wnttapp/src/AddressPopup.jsx
@@ -1,73 +1,41 @@
 import './css/AddressPopup.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useEffectEvent, useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import { Form } from 'react-bootstrap'
 import Spinner from 'react-bootstrap/Spinner'
 import Button from 'react-bootstrap/Button'
-import axios from 'axios'
-import { GeocodeUrl } from './utils'
-
-const Mode = Object.freeze({
-    Ready: 1,
-    Loading: 2,
-    Error: 3,
-})
+import useGeocode from './useGeocode'
+import { useQueryClient } from '@tanstack/react-query'
 
 export default function AddressPopup({ onClose, setAddressMarker, station }) {
-    const [mode, setMode] = useState(Mode.Ready)
+    const queryClient = useQueryClient()
     const [addressValue, setAddressValue] = useState('')
-    const [error, setError] = useState('')
+
+    const { isPending, data, error } = useGeocode(station, addressValue)
+
+    const onQueryFinished = useEffectEvent(() => {
+        if (!isPending) {
+            // This is the only way I could get useQuery to NOT use cache. invalidateQueries
+            // does not work, it just forces a refetch and we get an endless loop.
+            queryClient.removeQueries({ queryKey: ['geocode'] })
+        }
+    })
+
+    useEffect(() => {
+        onQueryFinished()
+    }, [error?.message]) // TODO: Not sure why this works
+
+    useEffect(() => {
+        if (data) {
+            setAddressMarker({ lat: Number(data.lat), lng: Number(data.lon) })
+            onClose()
+        }
+    }, [data, setAddressMarker, onClose])
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const val = e.currentTarget.addressLookup.value
-        setMode(Mode.Loading)
-        setAddressValue(val)
-        setError('')
+        setAddressValue(e.currentTarget.addressLookup.value)
     }
-
-    const handleError = (message) => {
-        setError(message)
-        setMode(Mode.Error)
-    }
-
-    useEffect(() => {
-        const isInBounds = (lat, lon) => {
-            const minLat = Math.min(station.mapBounds[0][0], station.mapBounds[1][0])
-            const minLon = Math.min(station.mapBounds[0][1], station.mapBounds[1][1])
-            const maxLat = Math.max(station.mapBounds[0][0], station.mapBounds[1][0])
-            const maxLon = Math.max(station.mapBounds[0][1], station.mapBounds[1][1])
-            return lat >= minLat && lat <= maxLat && lon >= minLon && lon <= maxLon
-        }
-
-        if (addressValue && /\S/.test(addressValue)) {
-            const lookupValue = addressValue + ' USA'
-            const encoded = lookupValue.replace(/\s+/gi, '+')
-            const url =
-                GeocodeUrl + '/search?q=' + encoded + '&api_key=' + import.meta.env.VITE_GEOCODE_KEY
-            axios
-                .get(url)
-                .then((res) => {
-                    if (res.data !== undefined && res.data[0] !== undefined) {
-                        const lat = res.data[0].lat
-                        const lon = res.data[0].lon
-                        if (isInBounds(lat, lon)) {
-                            setAddressMarker({ lat: Number(lat), lng: Number(lon) })
-                            onClose()
-                        } else {
-                            handleError('That address does not appear to be in the local area')
-                        }
-                    } else {
-                        handleError('That appears to be an invalid address')
-                    }
-                })
-                .catch((error) => {
-                    console.error(JSON.stringify(error))
-                    handleError('An error occurred. Please try again.')
-                })
-        }
-        // TODO: improve these dependencies
-    }, [addressValue, setAddressMarker, onClose, station.mapBounds])
 
     return (
         <Modal show={true} onHide={onClose}>
@@ -77,10 +45,10 @@ export default function AddressPopup({ onClose, setAddressMarker, station }) {
             <Modal.Body className='address-body'>
                 <Form onSubmit={(e) => handleSubmit(e)}>
                     <Form.Group className='mb-3' controlId='addressLookup'>
-                        {mode === Mode.Loading ? (
+                        {isPending && !!addressValue ? (
                             <Spinner animation='border' variant='primary' />
                         ) : (
-                            <p className='text-danger'>{error}</p>
+                            <p className='text-white'>{error?.message}</p>
                         )}
                         <Form.Control
                             name='addr'
@@ -94,7 +62,10 @@ export default function AddressPopup({ onClose, setAddressMarker, station }) {
                             Must be in the local area. Include city and state.
                         </Form.Text>
                     </Form.Group>
-                    <Button variant='custom-primary' type='submit' disabled={mode === Mode.Loading}>
+                    <Button
+                        variant='custom-primary'
+                        type='submit'
+                        disabled={isPending && !!addressValue}>
                         Search
                     </Button>
                 </Form>
