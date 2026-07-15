2f78903166dded75259b2271025c297d80c64e98
Gordon Shannon <gordo169@gmail.com>
2026-07-15 15:36:06 -0400

    refactored getDates css layout, now supports phone

--- wnttapp/src/GetDates.jsx
+++ wnttapp/src/GetDates.jsx
@@ -2,9 +2,8 @@ import './css/GetDates.css'
 import { useContext } from 'react'
 import HousePic from './images/housepic2.png?inline'
 import Button from 'react-bootstrap/Button'
-import { Col, Form, FormLabel, FormText, Row } from 'react-bootstrap'
+import { Form, FormLabel, FormText } from 'react-bootstrap'
 import { DatePicker } from 'reactstrap-date-picker'
-import Container from 'react-bootstrap/Container'
 import {
     addDays,
     daysBetween,
@@ -40,17 +39,13 @@ export default function GetDates({
         toggleHiloMode()
     }
 
-    const handleSubmit = (e) => {
-        // The form has 2 submit buttons -- refresh & reset, so handle them both here.
-        e.preventDefault()
-        const clickedButton = e.nativeEvent.submitter
+    const handleRefresh = () => {
+        // This will force a re-render even if the dates are the same as before
+        setDateRangeStrings(stringify(startCtl.start), stringify(endCtl.end), true)
+    }
 
-        if (clickedButton.name === 'refresh') {
-            // This will force a re-render even if the dates are the same as before
-            setDateRangeStrings(stringify(startCtl.start), stringify(endCtl.end), true)
-        } else if (clickedButton.name === 'reset') {
-            resetDateControls() // Let parent reset the date controls, and the appContext.
-        }
+    const handleReset = () => {
+        resetDateControls() // Let parent reset the date controls, and the appContext.
     }
 
     const handleStartChange = (formatted) => {
@@ -83,104 +78,91 @@ export default function GetDates({
     }
 
     return (
-        <Container id='get-dates' className='my-2'>
-            <Row className='align-items-center'>
-                <Col className='px-0'>
-                    <Row className='align-items-center mx-0 date-options'>
-                        <Col className='align-self-start'>
-                            <FormLabel>Start Date: </FormLabel>
-                            <DatePicker
-                                id='start-datepicker'
-                                showClearButton={false}
-                                showTodayButton={true}
-                                dateFormat='MM/DD/YYYY'
-                                value={stringify(startCtl.start)}
-                                minDate={stringify(startCtl.min)}
-                                maxDate={stringify(startCtl.max)}
-                                onChange={(_, f) => handleStartChange(f)}
-                            />
-                            <FormText muted>
-                                Range: {rangeMin} - {rangeMax}
-                            </FormText>
-                        </Col>
-                        <Col className='align-self-start'>
-                            <FormLabel>End Date: </FormLabel>
-                            <DatePicker
-                                id='end-datepicker'
-                                showClearButton={false}
-                                dateFormat='MM/DD/YYYY'
-                                value={stringify(endCtl.end)}
-                                minDate={stringify(endCtl.min)}
-                                maxDate={stringify(endCtl.max)}
-                                onChange={(_, f) => handleEndChange(f)}
-                            />
-                            <FormText muted>Maximum {getMaxNumDays()} day range</FormText>
-                        </Col>
+        <div id='get-dates'>
+            <div className='get-dates-range'>
+                <div>
+                    <FormLabel>Start Date: </FormLabel>
+                    <DatePicker
+                        id='start-datepicker'
+                        showClearButton={false}
+                        showTodayButton={true}
+                        dateFormat='MM/DD/YYYY'
+                        value={stringify(startCtl.start)}
+                        minDate={stringify(startCtl.min)}
+                        maxDate={stringify(startCtl.max)}
+                        onChange={(_, f) => handleStartChange(f)}
+                    />
+                    <FormText muted>
+                        Range: {rangeMin} - {rangeMax}
+                    </FormText>
+                </div>
+                <div>
+                    <FormLabel>End Date: </FormLabel>
+                    <DatePicker
+                        id='end-datepicker'
+                        showClearButton={false}
+                        dateFormat='MM/DD/YYYY'
+                        value={stringify(endCtl.end)}
+                        minDate={stringify(endCtl.min)}
+                        maxDate={stringify(endCtl.max)}
+                        onChange={(_, f) => handleEndChange(f)}
+                    />
+                    <FormText muted>Maximum {getMaxNumDays()} day range</FormText>
+                </div>
+            </div>
 
-                        <Col>
-                            <Row className='mx-0'>
-                                <Form onSubmit={handleSubmit}>
-                                    <Col className='d-flex align-items-center justify-content-center'>
-                                        <Overlay
-                                            text='Redraw the graph with the latest data using the selected date range.'
-                                            placement='top'
-                                            contents={
-                                                <Button
-                                                    variant='custom-primary'
-                                                    className='m-1'
-                                                    type='submit'
-                                                    name='refresh'>
-                                                    Refresh
-                                                </Button>
-                                            }></Overlay>
-                                        <Overlay
-                                            text='Return to the default date range and refresh the graph.'
-                                            placement='top'
-                                            contents={
-                                                <Button
-                                                    variant='custom-primary'
-                                                    className='m-1'
-                                                    type='submit'
-                                                    name='reset'>
-                                                    Reset
-                                                </Button>
-                                            }></Overlay>
-                                    </Col>
-                                </Form>
-                            </Row>
-                            <Row className='mx-0'>
-                                <Col className='d-flex align-items-center justify-content-center'>
-                                    <Overlay
-                                        text='Turn on to show only high and low tides.'
-                                        placement='top'
-                                        contents={
-                                            <Form>
-                                                <Form.Check
-                                                    type='switch'
-                                                    label='Highs/Lows'
-                                                    checked={isHiloMode}
-                                                    onChange={handleHiloToggle}
-                                                    disabled={isSmallScreen()}
-                                                />
-                                            </Form>
-                                        }></Overlay>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col sm={3} className='mx-0 py-2 text-center'>
+            <div className='get-dates-ctl'>
+                <div className='get-dates-buttons'>
                     <Overlay
-                        text='Add the elevation of your place of interest to the graph.'
+                        text='Redraw the graph with the latest data using the selected date range.'
                         placement='top'
                         contents={
-                            <div className='mx-md-2 pointer' onClick={() => onMapRequest()}>
-                                <img src={HousePic} width={75} alt='Map popup' />
-                                <div className='map-label'>Find my house!</div>
-                            </div>
+                            <Button variant='custom-primary' onClick={handleRefresh}>
+                                Refresh
+                            </Button>
                         }></Overlay>
-                </Col>
-            </Row>
-        </Container>
+                    <Overlay
+                        text='Return to the default date range and refresh the graph.'
+                        placement='top'
+                        contents={
+                            <Button variant='custom-primary' onClick={handleReset}>
+                                Reset
+                            </Button>
+                        }></Overlay>
+                </div>
+                <div className='hilo-box'>
+                    <Overlay
+                        text='Turn on to show only high and low tides.'
+                        placement='top'
+                        contents={
+                            <Form>
+                                <Form.Check
+                                    type='switch'
+                                    label='Highs/Lows'
+                                    checked={isHiloMode}
+                                    onChange={handleHiloToggle}
+                                    disabled={isSmallScreen()}
+                                />
+                            </Form>
+                        }></Overlay>
+                </div>
+            </div>
+            <Overlay
+                text='Add the elevation of your place of interest to the graph.'
+                placement='top'
+                contents={
+                    <div className='get-dates-custom pointer'>
+                        <img
+                            src={HousePic}
+                            width={50}
+                            alt='Map popup'
+                            onClick={() => onMapRequest()}
+                        />
+                        <div className='map-label' onClick={() => onMapRequest()}>
+                            Find my house!
+                        </div>
+                    </div>
+                }></Overlay>
+        </div>
     )
 }
