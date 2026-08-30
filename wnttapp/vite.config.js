a852185bd07614b2972f6def41c43ada6ece3600
Gordon Shannon <gordo169@gmail.com>
2026-08-30 14:45:04 -0400

    replace vitejs swc plugin

--- wnttapp/vite.config.js
+++ wnttapp/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import Inspect from 'vite-plugin-inspect'
 import eslint from 'vite-plugin-eslint2'
 
